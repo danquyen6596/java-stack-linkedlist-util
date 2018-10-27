161da87a56a1037fcdb9d19cd34f4cfafaa1d99a
Quyen Le Sy <danquyen6596@gmail.com>
2018-10-27 14:08:30 +0700

    Add file to program.

---
+++ src/vn/codegym/QuyenLeSy/Main.java
@@ -0,0 +1,11 @@
+package vn.codegym.QuyenLeSy;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("1. Stack of integers");
+        stackOfIntegers();
+        System.out.println("\n2. Stack of Strings");
+        stackOfIStrings();
+    }
+}
